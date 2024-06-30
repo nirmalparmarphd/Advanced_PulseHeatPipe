4ddf6aa7a24a9d5e255ef98cf17b0dbb2c8aeab5
Nirmal Parmar <nirmalparmarphd@gmail.com>
2024-06-30 15:02:41 +0200

    di pipeline updated

--- ml/pipelines/data_ingestion_pipe.py
+++ ml/pipelines/data_ingestion_pipe.py
@@ -12,13 +12,22 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from steps.data_ingestion import step_data_ingestion, step_data_join, step_electrical_data_cleaning, step_thermal_data_cleaning
+from steps.data_ingestion import (step_data_ingestion, 
+                                  step_data_join, 
+                                  step_electrical_data_dt_process, 
+                                  step_thermal_data_dt_process, 
+                                  step_get_file_list, 
+                                  step_initialization,
+                                  step_cleaning_data)
 
 # individual thermal and electrical data ingestion pipeline
 @pipeline(enable_cache=False)
-def data_ingestion_pipeline(path):
-    df_thermal, df_electrical = step_data_ingestion(path)
-    df_thermal_clean = step_thermal_data_cleaning(path, df_thermal=df_thermal)
-    df_electrical_clean = step_electrical_data_cleaning(path, df_electrical=df_electrical)
-    df_join = step_data_join(path, df_thermal=df_thermal_clean, df_electrical=df_electrical_clean)
-    return df_join
+def data_ingestion_pipeline(dir_path):
+    di = step_initialization(path=dir_path)
+    file_list = step_get_file_list(di=di)
+    df_thermal, df_electrical = step_data_ingestion(file_list, di=di)
+    df_thermal_clean = step_thermal_data_dt_process(df_thermal=df_thermal, di=di)
+    df_electrical_clean = step_electrical_data_dt_process(df_electrical=df_electrical, di=di)
+    df_join = step_data_join(df_thermal=df_thermal_clean, df_electrical=df_electrical_clean, di=di)
+    df_join_clean = step_cleaning_data(df_join, di=di)
+    return df_join_clean
--- ml/steps/data_ingestion.py
+++ ml/steps/data_ingestion.py
@@ -6,12 +6,15 @@ import numpy as np
 import chardet
 from typing import Annotated, Tuple, NoReturn
 from zenml import step
+import glob
 
 
 class DataIngestion:
     """
     DataIngestion is used for various methods as mentioned below
 
+        # get a list of each experimental files
+    
         # identify utf type
 
         # load csv (thermal and electrical)
@@ -19,59 +22,102 @@ class DataIngestion:
         # handle date-time cols, basic data cleaning
 
         # join thermal and electrical data on date-time cols
-    """
-    
-    def __init__(self, path:str) -> None:
 
-        self.path = path
-        if self.path.split('_')[1]=='E':
-            self.path_thermal = self.path.replace('_E_', '_T_')
-            self.path_electrical = self.path
-        elif self.path.split('_')[1]=='T':
-            self.path_electrical = self.path.replace('_T_', '_E_')
-            self.path_thermal = self.path
-        else:
-            raise ValueError('### check raw data file name!')
-        
+        # combine all experimental data to create a database
+    """
+
+    def __init__(self, data_directory:str='../data/'):
+        """
+        initialization of the class DataIngestion
+
+        Arg:
+            data_directory:str
+
+        Returns:
+            None
+        """
+        self.data_path = data_directory
+
+    def get_list_files(self, name:str='*', ext:str=".csv")->Annotated[list, 'file list']:
+        """
+        to get a list of experimental files names
+
+        Arg:
+            name:str "*"
+            ext:str ".csv"
+
+        Return:
+            List of experimental file
+        """
+        self.name = name
+        self.ext = ext
+        data_filenames_list = glob.glob((self.data_path + self.name + self.ext))
+
+        file_list = []
+        for file in data_filenames_list:
+            if 'T' in file.split('_'):
+                file_list.append(file)
+                
+        return file_list
+
+        return data_filenames_list
+        # df_frames = []
+        # for i in data_filenames_list:
+        #     df = pd.read_csv(data_filenames_list[i])
+        #     df_frames.append(df)
+        # df = pd.concat(df_frames, axis=0, ignore_index=True)
+
     # identify utf type
-    def identify_utf_type(self)-> Annotated[str, 'UTF-type']:
+    def identify_utf_type(self, path)-> Annotated[str, 'UTF-type']:
         """
         to identify UTF type and feed to csv load function.
         for internal calling
         
         args:
-            None
+            path:str # file path
 
         use:
             utf_type = identify_utf_type()
 
         """
         # Detect the encoding
-        with open(self.path, "rb") as file:
+        with open(path, "rb") as file:
             result = chardet.detect(file.read())
             utf_type = result['encoding']
-        
         return utf_type
 
     # load csv (thermal and electrical)
-    def loading_csv_data(self, utf_type:str)->Tuple[Annotated[pd.DataFrame, 'RAW Data Thermal'],
+    def loading_csv_data(self, utf_type:str, path:str)->Tuple[Annotated[pd.DataFrame, 'RAW Data Thermal'],
                                                     Annotated[pd.DataFrame, 'Raw Data Electrical']]:
         """
         to load experimental csv data for each Q[W] heat and Alpha-Beta combinations.
 
         args:
             utf_type:str # 'UTF-16'
+            path:str # file path
 
         use:
             df = loading_csv_data(utf_type='UTF_16')
 
         """
+
+        self.path = path
+        if 'E' in self.path.split('_')[1]:
+            self.path_thermal = self.path.replace('_E_', '_T_')
+            self.path_electrical = self.path
+        elif 'T' in self.path.split('_')[1]:
+            self.path_electrical = self.path.replace('_T_', '_E_')
+            self.path_thermal = self.path
+        else:
+            raise ValueError('### check raw data file name!')
         df_thermal = pd.read_csv(self.path_thermal, encoding=utf_type ,delimiter='\t')
         df_electrical = pd.read_csv(self.path_electrical, encoding=utf_type ,delimiter='\t')
         return df_thermal, df_electrical
 
     # handle date-time cols, basic data cleaning
-    def processing_date_time(self, df:pd.DataFrame, col:str='date', format:str='%d/%m/%Y%H:%M:%S')-> Annotated[pd.DataFrame,'Process Data']:
+    def processing_date_time(self, df:pd.DataFrame, 
+                             col:str='date', 
+                             format:str='%d/%m/%Y%H:%M:%S')-> Annotated[pd.DataFrame,'Process DateTime col']:
         """
         to process date time cols from row data and returns timestamp col for further analysis
 
@@ -87,47 +133,80 @@ class DataIngestion:
         df[col] = df['DATE'] + df['TIME']
         df[col] = pd.to_datetime(df[col], format=format)
         return df
-    
+
     # join thermal and electrical data on date-time cols
-    def join_thermal_electrical_df(self, df_thermal:pd.DataFrame, df_electrical:pd.DataFrame) -> Annotated[pd.DataFrame, 'Joined Data']:
+    def join_thermal_electrical_df(self, df_thermal:pd.DataFrame, df_electrical:pd.DataFrame) -> Annotated[pd.DataFrame, 'Joined All Experimental Data']:
         """
         to join cleaned thermal and electrical data sets for each set of experiment
 
         args:
-
-        use:
+            df_thermal:pd.DataFrame
+            df_electrical:pd.DataFrame
+        
+        return:
+            df_joined:pd.DataFrame
 
         """
-        df_join = pd.merge(left=df_thermal, right=df_electrical, on=['date', 'TIME', 'DATE'])
-        df_join.rename(columns=lambda x: x.replace('-', '_'), inplace=True)
-        df_join.dropna(axis=0, how='any', inplace=True)
-        df_join.dropna(axis=1, how='all', inplace=True)
+        df_join = pd.merge(left=df_thermal, right=df_electrical, on=['date', 'TIME', 'DATE'], how='inner')
         return df_join
 
+    def cleaning_data(self, df_join:pd.DataFrame):
+        """
+        to clean joined data, drop rows with Nan or 0, removing false entries, etc.
 
+        args:
+            df:pd.DataFrame
+
+        returns:
+            df:pd.DataFrame
+        """
+
+        df_join.rename(columns=lambda x: x.replace('-', '_'), inplace=True)
+        df_join.dropna(axis=1, how='all', inplace=True)
+        df_join.dropna(axis=0, inplace=True)
+        df_join.fillna(0)
+        return df_join
 
 @step
-def step_data_ingestion(path:str)->Tuple[Annotated[pd.DataFrame, 'RAW Data Thermal'],
-                                        Annotated[pd.DataFrame, 'Raw Data Electrical']]:
-    di = DataIngestion(path=path)
-    utf_type = di.identify_utf_type()
-    df_thermal, df_electrical = di.loading_csv_data(utf_type=utf_type)
-    return df_thermal, df_electrical
+def step_initialization(path:str)->Annotated[DataIngestion, 'Data Ingestion Engine']:
+    ig = DataIngestion(data_directory=path)
+    return ig
+
+@step 
+def step_get_file_list(di:DataIngestion)->Annotated[list, 'List of Experimental Files']:
+    file_list = di.get_list_files()
+    return file_list
 
 @step
-def step_thermal_data_cleaning(path:str, df_thermal:pd.DataFrame)->Annotated[pd.DataFrame,'Cleaned Thermal Data']:
-    di = DataIngestion(path=path)
+def step_data_ingestion(file_list:list, di:DataIngestion)->Tuple[Annotated[pd.DataFrame, 'RAW Data Thermal'],
+                                                        Annotated[pd.DataFrame, 'Raw Data Electrical']]:
+    df_t = []
+    df_e = []
+    for file in file_list:
+        utf_type = di.identify_utf_type(path=file)
+        df_thermal, df_electrical = di.loading_csv_data(utf_type=utf_type, path=file)
+        df_t.append(df_thermal)
+        df_e.append(df_electrical)
+    df_thermal_combined = pd.concat(df_t, ignore_index=True)
+    df_electrical_combined = pd.concat(df_e, ignore_index=True)
+    return df_thermal_combined, df_electrical_combined
+
+@step
+def step_thermal_data_dt_process(df_thermal:pd.DataFrame, di:DataIngestion)->Annotated[pd.DataFrame,'Thermal Data - DT Process']:
     df_ = di.processing_date_time(df=df_thermal)
     return df_
 
 @step
-def step_electrical_data_cleaning(path:str, df_electrical:pd.DataFrame)->Annotated[pd.DataFrame,'Cleaned Electrical Data']:
-    di = DataIngestion(path=path)
+def step_electrical_data_dt_process(df_electrical:pd.DataFrame, di:DataIngestion)->Annotated[pd.DataFrame,'Electrical Data - DT Process']:
     df_ = di.processing_date_time(df=df_electrical)
     return df_
 
 @step
-def step_data_join(path, df_thermal:pd.DataFrame, df_electrical:pd.DataFrame)->Annotated[pd.DataFrame, 'Joined Data']:
-    di = DataIngestion(path=path)
+def step_data_join(df_thermal:pd.DataFrame, df_electrical:pd.DataFrame, di:DataIngestion)->Annotated[pd.DataFrame, 'Joined Data']:
     df_join = di.join_thermal_electrical_df(df_thermal=df_thermal, df_electrical=df_electrical)
     return df_join
+
+@step
+def step_cleaning_data(df_join:pd.DataFrame, di:DataIngestion)->Annotated[pd.DataFrame, 'Cleaning Joined Data']:
+    df_join_clean = di.cleaning_data(df_join=df_join)
+    return df_join_clean 
