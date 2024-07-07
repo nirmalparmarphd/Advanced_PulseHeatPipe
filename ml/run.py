fb23a6467c1a14d87c64f0cf174b800a499fc48e
Nirmal Parmar <nirmalparmarphd@gmail.com>
2024-07-07 18:23:38 +0200

    updated

--- ml/run.py
+++ ml/run.py
@@ -3,6 +3,9 @@
 from pipelines.data_ingestion_pipe import data_ingestion_pipeline, database_generation_pipeline
 
 from zenml import pipeline
+import sys, os
+
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 @pipeline(enable_cache=False, name='main_pipeline')
 def main_pipeline(path:str = '../data/'):
