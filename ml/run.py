9d4f2e4729b7ed2251445394906b39e70dc9c9b8
Nirmal Parmar <nirmalparmarphd@gmail.com>
2024-07-14 13:17:18 +0200

    added outline

--- ml/run.py
+++ ml/run.py
@@ -9,8 +9,19 @@ import sys, os
 
 @pipeline(enable_cache=False, name='main_pipeline')
 def main_pipeline(path:str = '../data/'):
+    # ingesting raw experimental data and combining them all
     data_ingestion = data_ingestion_pipeline(dir_path=path)
+
+    # with help of the experimental metal table selecting/filtering data, combining, and cleaning
     database_generation = database = database_generation_pipeline(dir_path=path)
+    
+    # auto generation of plots for selected thermal properties
+
+    # data pre-processing before ML
+
+    # ML training and evaluation of ML model
+
+    # auto generation of plots for ML-experimental data
 
 # running main pipeline
 main_pipeline()
