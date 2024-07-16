69614557cc77e945892f0cd40714cf4f4b9793e1
Nirmal Parmar <nirmalparmarphd@gmail.com>
2024-07-16 20:18:57 +0200

    run updated

--- ml/run.py
+++ ml/run.py
@@ -7,8 +7,8 @@ import sys, os
 
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-@pipeline(enable_cache=False, name='main_pipeline')
-def main_pipeline(path:str = '../data/'):
+@pipeline(enable_cache=False, name='main_pipeline_php')
+def main_pipeline_php(path:str = '../data/'):
     # ingesting raw experimental data and combining them all
     data_ingestion = data_ingestion_pipeline(dir_path=path)
 
@@ -24,5 +24,5 @@ def main_pipeline(path:str = '../data/'):
     # auto generation of plots for ML-experimental data
 
 # running main pipeline
-main_pipeline()
+main_pipeline_php()
 
